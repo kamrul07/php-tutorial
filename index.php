b956ea98c8ce4d76a425831f511fbfcdf7af7ec9
DESKTOP-6GE6FHU\kamru <kamrulhasanshuvo04@gmail.com>
2017-11-09 11:32:39 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html>
+    
+    <head>
+        
+        
+    </head>
+    <body style="margin:0 auto;width:960px">
+        
+        <section style="background-color:<?php echo "black";?>;height:100px;width:100%;margin:0 auto" id="header">
+            
+            <?php
+            $first = "I love Lisa";
+          
+            ?>
+            
+            
+            
+        </section>
+        <sextion id="content">
+            
+            <?php
+            $i=20;
+         for($j=$i;$j<25 and $j>0;$j--){
+             
+             echo $j;
+         }
+     
+           
+        
+           
+            
+            
+            
+            ?>
+            
+        </sextion>
+         <section style="background-color:<?php echo "black";?>;height:100px;width:100%;margin:0 auto" id="footer">
+            
+            
+            
+            
+            
+        </section>
+        
+        
+    </body>
+</html>
