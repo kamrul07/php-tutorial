c67682ea7cdec50b87e6cadcb9a20b7c32a6e5d7
DESKTOP-6GE6FHU\kamru <kamrulhasanshuvo04@gmail.com>
2017-11-13 20:55:53 +0600

    File

---
+++ date.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html>
+    
+    <head>
+    <title>Learn with me - array sourch code for practise</title>    
+    </head>
+    <body style="margin:0 auto;width:960px;background:#EAEAEA">
+        
+        <div style="background-color:<?php echo "white";?>;width:100%;margin:0 auto;overflow: hidden;" >
+        <div style="background-color:<?php echo "black";?>;overflow: hidden;" >
+            
+            <h2 style="color:white;text-align:center;font-size:48px;padding: 27px;">Php tutorial with shuvo</h2>
+            <h2 style="color: red;text-align: center;font-size: 28px;padding: 0px 3px 18px 0px;line-height: 0px;"><a href="http://kamrulhasanshuvo.info">with shuvo</a> </h2>
+        </div>
+        <div id="content" style="min-height:68vh;padding:20px">
+        <h2 style="font-size:38px;text-align:center">Date Function</h2>
+      
+        
+               <br>
+               <hr>
+               <br> 
+            <?php
+           echo "today's date is: " . date("y-m-d") . "<br>";
+           echo "today's time is: " . date("h-i-s") . "<br>";
+           echo "Timezone: " . date_default_timezone_get() . "<br>";
+            date_default_timezone_set("Asia/Dhaka");
+            echo "Bangladesh Date " . date("y-m-d") ." Time : " . date("h-i-sa");
+            ?>   
+
+        </div>
+       <div style="background-color:<?php echo "black";?>;width:100%;margin:0 auto;overflow: hidden;" id="header">
+            
+       
+            <p style="color: red;text-align: center;font-size: 28px;padding: 0px 3px 18px 0px;line-height: 0px;"><a href="http://kamrulhasanshuvo.info">with shuvo</a> </p>
+        </div>
+        </div>
+        
+    </body>
+</html>
